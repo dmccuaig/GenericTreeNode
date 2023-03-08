e9120510baef18413d0ba53e8d3ad9f4f45917a2
Dan McCuaig <dan@dantrace.org>
2023-03-07 21:59:15 -0800

    Start of treenode

---
+++ GenericTreeNode/GenericTreeNode.UnitTests/TreeNodeTests.cs
@@ -0,0 +1,117 @@
+﻿using NUnit.Framework;
+using GenericTreeNode;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design.Serialization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericTreeNode.Tests
+{
+	[TestFixture()]
+	public class TreeNodeTests
+	{
+		private static TreeNode<string> GetTestTree()
+		{
+			var A = new TreeNode<string>("A");
+				var B = A.AddChild("B");
+					var G = B.AddChild("G");
+					var H = B.AddChild("H");
+						var M = H.AddChild("M");
+						var N = H.AddChild("N");
+					var I = B.AddChild("I");
+				var C = A.AddChild("C");
+			  var D = A.AddChild("D");
+			  var E = A.AddChild("E");
+					var J = E.AddChild("J");
+					var K = E.AddChild("K");
+			  var F = A.AddChild("F");
+					var L = F.AddChild("L");
+
+			return A;
+		}
+
+		private static IEnumerable<string> GetTreeData(IEnumerable<TreeNode<string>> nodes)
+		{
+			return nodes.Select(node => node.Data);
+		}
+
+		private static string GetTreeString(IEnumerable<TreeNode<string>> nodes)
+		{
+			return string.Join("", GetTreeData(nodes));
+		}
+
+
+		[Test]
+		public static void When_ExaminingRoot_ExpectProperPlacing()
+		{
+
+			var A = GetTestTree();
+			Assert.IsTrue(A.IsRoot);
+			Assert.IsNull(A.Parent);
+			CollectionAssert.IsNotEmpty(A.Children);
+			Assert.AreEqual(A.Children.Count(), 5);
+			Assert.AreEqual(A.Count, 5);
+			Assert.AreEqual(A.Level, 0);
+
+			string descendents = GetTreeString(A.GetDescendents());
+			Assert.AreEqual(descendents, "BGHMNICDEJKFL");
+			string all = GetTreeString(A);
+			Assert.AreEqual(all, "ABGHMNICDEJKFL");
+			string children = GetTreeString(A.Children);
+			Assert.AreEqual(children, "BCDEF");
+		}
+
+		[Test]
+		public static void When_ExaminingMiddleNode_ExpectProperPlacing()
+		{
+			var A = GetTestTree();
+
+			var H = A.FirstOrDefault(n => string.Equals(n.Data, "H"));
+			Assert.IsNotNull(H);
+			CollectionAssert.IsNotEmpty(H.Children);
+			Assert.AreEqual(H.Children.Count(), 2);
+			Assert.AreEqual(H.Count, 2);
+			Assert.IsFalse(H.IsRoot);
+			Assert.IsFalse(H.IsLeaf);
+			Assert.AreEqual(H.Level, 2);
+
+			var B = A.FirstOrDefault(n => string.Equals(n.Data, "B"));
+			Assert.AreEqual(H.Parent, B);
+
+			string descendents = GetTreeString(H.GetDescendents());
+			Assert.AreEqual(descendents, "MN");
+			string all = GetTreeString(H);
+			Assert.AreEqual(all, "HMN");
+			string children = GetTreeString(H.Children);
+			Assert.AreEqual(children, "MN");
+		}
+
+		[Test]
+		public static void When_ExaminingLeafNode_ExpectProperPlacing()
+		{
+			var A = GetTestTree();
+
+			var M = A.FirstOrDefault(n => string.Equals(n.Data, "M"));
+			Assert.IsNotNull(M);
+			CollectionAssert.IsEmpty(M.Children);
+			Assert.IsFalse(M.IsRoot);
+			Assert.IsTrue(M.IsLeaf);
+			Assert.AreEqual(M.Level, 3);
+			Assert.AreEqual(M.Children.Count(), 0);
+			Assert.AreEqual(M.Count, 0);
+
+			var H = A.FirstOrDefault(n => string.Equals(n.Data, "H"));
+			Assert.AreEqual(M.Parent, H);
+
+			string descendents = GetTreeString(M.GetDescendents());
+			Assert.AreEqual(descendents, "");
+			string all = GetTreeString(M);
+			Assert.AreEqual(all, "M");
+			string children = GetTreeString(M.Children);
+			Assert.AreEqual(children, "");
+		}
+
+	}
+}
---
+++ GenericTreeNode/GenericTreeNode/TreeNode.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GenericTreeNode
+{
+	public class TreeNode<T> : IEnumerable<TreeNode<T>>, ITreeNode
+	{
+		private T _data;
+
+		public T Data
+		{
+			get => _data;
+			set
+			{
+				if (IsReadOnly == false)
+				{
+					_data = value;
+				}
+			}
+		}
+
+		public TreeNode<T> Parent { get; protected set; }
+
+		private List<TreeNode<T>> _children;
+		public IEnumerable<TreeNode<T>> Children => _children ?? Enumerable.Empty<TreeNode<T>>();
+		private List<TreeNode<T>> ChildList => _children ?? (_children = new List<TreeNode<T>>());
+
+		public bool IsRoot => Parent == null;
+		public bool IsLeaf => _children is null || _children.Count == 0;
+		public bool IsReadOnly { get; set; }
+		public int Count => _children?.Count ?? 0;
+		public int Level => this.IsRoot ? 0 : Parent.Level + 1;
+
+		public TreeNode(T data, bool isReadOnly = false)
+		{
+			_data = data;
+			IsReadOnly = isReadOnly;
+		}
+
+		public TreeNode<T> AddChild(T data, bool isReadOnly = false)
+		{
+			var node = new TreeNode<T>(data, isReadOnly);
+			AddChild(node);
+			return node;
+		}
+
+		public void AddChild(TreeNode<T> child) => OnAddChild(child);
+
+		protected virtual void OnAddChild(TreeNode<T> child)
+		{
+			ChildList.Add(child);
+			child.Parent = this;
+		}
+
+		public TreeNode<T> InsertChild(int index, T data, bool isReadOnly = false)
+		{
+			var node = new TreeNode<T>(data, isReadOnly);
+			return InsertChild(index, node) ? node : null;
+		}
+
+		public bool InsertChild(int index, TreeNode<T> child) => OnInsertChild(index, child);
+
+		protected virtual bool OnInsertChild(int index, TreeNode<T> child)
+		{
+			if (index < 0 || index >= ChildList.Count) return false;
+			ChildList.Insert(index, child);
+			child.Parent = this;
+			return true;
+		}
+
+		public bool RemoveChild(T data)
+		{
+			TreeNode<T> child = Children.FirstOrDefault(c => Equals(c.Data, data));
+			if (child == null) return false;
+			return ChildList.Remove(child);
+		}
+
+		public bool RemoveChild(TreeNode<T> child) => OnRemoveChild(child);
+
+		protected virtual bool OnRemoveChild(TreeNode<T> child)
+		{
+			child.Parent = null;
+			if (_children != null && _children.Count > 0 && _children.Remove(child))
+			{
+				child.Parent = null;
+				return true;
+			}
+
+			return false;
+		}
+
+		public override string ToString() => Data?.ToString() ?? GetType().Name;
+
+		#region iterating
+
+		public IEnumerable<TreeNode<T>> BreadthFirst()
+		{
+			yield return this;
+			foreach (TreeNode<T> node in Children.SelectMany(c => c.BreadthFirst()))
+			{
+				yield return node;
+			}
+		}
+
+		public IEnumerable<TreeNode<T>> DepthFirst()
+		{
+			foreach (TreeNode<T> node in Children.SelectMany(c => c.BreadthFirst()))
+			{
+				yield return node;
+			}
+
+			yield return this;
+		}
+
+		public IEnumerable<TreeNode<T>> GetPath()
+		{
+			return AsEnumerable().Union(GetPath()); // will it work?
+		}
+
+		public IEnumerable<TreeNode<T>> GetDescendents()
+		{
+			return this.Skip(1);
+		}
+
+		private IEnumerable<TreeNode<T>> AsEnumerable()
+		{
+			yield return this;
+		}
+
+
+
+		//public void Clear()
+		//{
+		//	_children.Clear();
+		//}
+
+		//public TreeNode<T> FindInChildren(T data)
+		//{
+		//	int i = 0, l = Count;
+		//	for (; i < l; ++i)
+		//	{
+		//		TreeNode<T> child = _children[i];
+		//		if (child.Data.Equals(data)) return child;
+		//	}
+
+		//	return null;
+		//}
+
+
+		#endregion
+
+		#region iterating
+
+		IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+		public IEnumerator<TreeNode<T>> GetEnumerator()
+		{
+			yield return this;
+			foreach (var directChild in this.Children)
+			{
+				foreach (var anyChild in directChild)
+					yield return anyChild;
+			}
+		}
+
+		#endregion
+
+	}
+
+	public interface ITreeNode
+	{
+	}
+
+	public class SelectionTreeNode<T> : TreeNode<T>
+	{
+		public bool IsSelected { get; set; }
+		public bool IsChildSelected { get; set; }
+		public bool IsExpanded { get; set; }
+
+
+		public SelectionTreeNode(T data) : base(data)
+		{
+		}
+
+
+	}
+}
